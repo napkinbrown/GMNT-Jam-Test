062b4df9910f0512042286c4d278c2a222ba9b2b
Dominic Brown <domingomonk@gmail.com>
2018-08-19 18:33:43 -0400

    Added ball prefab and functionality. This closes #4

---
+++ GameJamTestProject/Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	
+	public float maxSpeed;
+	public float initSpeed;
+	public Rigidbody2D rb;
+	
+	private float currentSpeed;	
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		currentSpeed = initSpeed;
+		
+		rb.AddForce(Vector2.one * currentSpeed, ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.Log(rb.velocity.magnitude);
+		
+		if (rb.velocity.magnitude > maxSpeed) 
+		{
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+	}
+}
