e4e771c34e10e31d6bcc100aab5a460ad97e83bc
Dalton Teague <dnteague@ncsu.edu>
2018-08-24 19:51:59 -0400

    Ball starts with paddle, launches on click

--- GameJamTestProject/Assets/Scripts/BallController.cs
+++ GameJamTestProject/Assets/Scripts/BallController.cs
@@ -7,15 +7,21 @@ public class BallController : MonoBehaviour {
 	public float maxSpeed;
 	public float initSpeed;
 	public Rigidbody2D rb;
+
+    public float rangeMin;
+    public float rangeMax;
 	
-	private float currentSpeed;	
+	private float currentSpeed;
+
+    private Vector2 initVector;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		currentSpeed = initSpeed;
-		
-		rb.AddForce(Vector2.one * currentSpeed, ForceMode2D.Impulse);
+        rangeMin = -.75f;
+        rangeMax = .75f;
+        initVector = new Vector2(Random.Range(rangeMin, rangeMax), Random.value);
 	}
 	
 	// Update is called once per frame
@@ -24,5 +30,14 @@ public class BallController : MonoBehaviour {
 		{
 			rb.velocity = rb.velocity.normalized * maxSpeed;
 		}
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            transform.parent = null;
+
+            //rb.AddForce(new Vector2(Random.Range(-1, 1), Random.value) * currentSpeed, ForceMode2D.Impulse);
+            rb.AddForce(initVector * currentSpeed 
+                        / Mathf.Sqrt(Mathf.Pow(initVector.x, 2) 
+                                     + Mathf.Pow(initVector.y, 2)), ForceMode2D.Impulse);
+        }
 	}
 }
