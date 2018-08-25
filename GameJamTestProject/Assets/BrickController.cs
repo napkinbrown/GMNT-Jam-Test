88aaa92f895caffd485cc7877e446b204eee4ee8
Dalton Teague <dnteague@ncsu.edu>
2018-08-24 20:48:13 -0400

    brick changes sprites on collision, disables at 0 health

---
+++ GameJamTestProject/Assets/BrickController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickController : MonoBehaviour {
+
+    //Possibly switch between sprites 
+    public int brickHealthMax;
+    private int brickHealth;
+
+    public Sprite wornBrick;
+    public Sprite nearBrokenBrick;
+
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        brickHealth = brickHealthMax;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        brickHealth--;
+        if (brickHealth == 0) {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (brickHealth == brickHealthMax - 1) {
+            spriteRenderer.sprite = wornBrick;
+        } else if (brickHealth == 1) {
+            spriteRenderer.sprite = nearBrokenBrick;
+        }
+		
+	}
+}
