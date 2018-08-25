abd28aff60b67dacf1332fd33c2c171c36de9e84
Tocino <Bobinbacon@yahoo.com>
2018-08-24 20:47:12 -0400

    Adding some starting vaules

--- GameJamTestProject/Assets/Scripts/GameController.cs
+++ GameJamTestProject/Assets/Scripts/GameController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+	public int lvl;
+	public int playerScore;
+	public int playerLives;
+	
 	// Use this for initialization
 	void Start () {
-		
+		lvl = 1;
+		playerScore = 0;
+		playerLives = 3;
+        	//SpawnBricks();
 	}
 	
 	// Update is called once per frame
